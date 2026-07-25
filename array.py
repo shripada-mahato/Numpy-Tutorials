9bcd3712399a7cdbee14730cd623dfa1fa824603
Shripada Mahato <mahatoshripada692@gmail.com>
2026-07-25 11:01:21 +0530

    Numpy Tutorials folder connect with github

---
+++ array.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = [10,20,30,40]
+
+arr1 = np.array(arr)
+print("it is a Numpy Array:- ", arr1)
+print("noraml array:- ", arr)
---
+++ hello.py
@@ -0,0 +1,7 @@
+import numpy as np
+count = 0
+arr = [10,20,30]
+for i in arr:
+    count += 1
+    print(i)
+    print(count)
